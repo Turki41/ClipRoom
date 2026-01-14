5f59880d11da1daccf9e188baf4987800debe5a3
Turki <turkisalh97@gmail.com>
2026-01-14 14:31:42 +0300

    auth layout setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const layout = () => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>layout</div>
+    <main className='auth-layout'>
+      <aside className='testimonial'>
+        <header>
+          <Link href={'/'}>
+            <Image src={'/assets/icons/logo.svg'} alt={'logo'} width={32} height={32} />
+            <h1>ClipRoom</h1>
+          </Link>
+        </header>
+
+        <div className='description'>
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Image src={'/assets/icons/star.svg'} alt='star' width={20} height={20} key={index} />
+              ))}
+            </figure>
+            <p>ClipRoom is your all-in-one space to capture your screen, record clips, and host your videos effortlessly. ClipRoom makes recording and sharing simple, fast, and organized.</p>
+          </section>
+        </div>
+
+        <footer>
+          <p>©ClipRoom. {new Date().getFullYear()}</p>
+        </footer>
+
+      </aside>
+
+      <aside>
+        {children}
+      </aside>
+      
+    </main>
   )
 }
 
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+    return (
+        <main>
+            sign in
+        </main>
+    )
+}
+
+export default page
