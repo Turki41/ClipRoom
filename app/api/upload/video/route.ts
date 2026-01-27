c45e62a0499673677dbeca7e8ea0db2610a2c38a
Turki <turkisalh97@gmail.com>
2026-01-27 18:35:06 +0300

    upload video api implemented

--- app/api/upload/video/route.ts
+++ app/api/upload/video/route.ts
@@ -11,12 +11,18 @@ export const POST = async (req: Request) => {
         const thumbnail = formData.get('thumbnail')
         const duration = formData.get('duration')
 
-        // upload video to bucket -> upload thumbnail to bucket -> store video metadata in db (with urls to video and thumbnail from the bucket)
-
         const supabase = await createClient()
 
+        // Auth check (get user id)
+        const { data: authData } = await supabase.auth.getUser()
+        const user = authData.user
+        if (!user) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+        }
+
+
         if (!(video instanceof File) || !(thumbnail instanceof File)) {
-            return NextResponse.json({message: 'Invalid video or thumbnail file'}, {status: 400})
+            return NextResponse.json({ message: 'Invalid video or thumbnail file' }, { status: 400 })
         }
 
         // upload video
@@ -24,7 +30,7 @@ export const POST = async (req: Request) => {
 
         if (videoError) {
             console.error('Error uploading video in upload video api controller:', videoError)
-            return NextResponse.json({message: 'Failed to upload video'}, {status: 500})
+            return NextResponse.json({ message: 'Failed to upload' }, { status: 500 })
         }
 
         // upload thumbnail
@@ -32,12 +38,39 @@ export const POST = async (req: Request) => {
 
         if (thumbnailError) {
             console.error('Error uploading thumbnail in upload video api controller:', thumbnailError)
-            return NextResponse.json({message: 'Failed to upload thumbnail'}, {status: 500})
+
+            const { error } = await supabase.storage.from('Videos').remove([videoData.path]) // rollback video upload
+            if (error) {
+                console.error('Error rolling back video upload after thumbnail upload failure:', error)
+            }
+
+            return NextResponse.json({ message: 'Failed to upload' }, { status: 500 })
         }
 
-        return NextResponse.json({message: {success: true}}, {status: 200})
+        const { data: signedVideo } = await supabase.storage.from('Videos').createSignedUrl(videoData.path, 60 * 60 * 24 * 365) // 1 year expiry
+        const { data: signedThumbnail } = await supabase.storage.from('Thumbnails').createSignedUrl(thumbnailData.path, 60 * 60 * 24 * 365) // 1 year expiry
+
+
+        // store video metadata in db
+        const { data: videoMetaData, error: videoMetaError } = await supabase.from('Videos').insert({
+            user_id: user.id,
+            title,
+            description,
+            visibility,
+            video_url: signedVideo?.signedUrl,
+            thumbnail_url: signedThumbnail?.signedUrl,
+            duration: duration,
+        })
+
+        if (videoMetaError) {
+            console.error('Error storing video metadata in upload video api controller:', videoMetaError)
+            return NextResponse.json({ message: 'Failed to upload' }, { status: 400 })
+        }
+
+        return NextResponse.json({ message: { success: true } }, { status: 201 })
+
     } catch (error) {
         console.error('Error uploading video in upload video api controller:', error)
-        return NextResponse.json({message: 'Internal Server Error'}, {status: 500})
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 })
     }
 }
